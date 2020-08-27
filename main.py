231abf4e1de26e8ed3aeebd5c45282991a513fa5
Joshua E. Schwartz <josh.schwartz92@gmail.com>
2020-08-27 14:13:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ EMAIL_CREDENTIALS = {
 
 
 def get_args() -> object:
-    parser = argparse.ArgumentParser(description="Key phrase search")
+    parser = argparse.ArgumentParser(description="Cipher key and helper arguments")
     parser.add_argument("-key", required=True, type=str,
                         help='cipher_key')
     parser.add_argument("-pdf", required=False, type=bool,
